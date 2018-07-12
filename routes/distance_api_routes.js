ff207a7ba87bc9883d561510b898c2d957c20e1c
isaac1104 <isaac-kwon@hotmail.com>
2018-07-11 19:51:43 -0700

    refactor & key hidden

--- routes/distance_api_routes.js
+++ routes/distance_api_routes.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
 const axios = require('axios');
 
 module.exports = app => {
   app.get('/api/distance', async (req, res) => {
-    const request = await axios.get(`https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=9979+Muirlands+Blvd,%20Irvine,%20CA%2092618&destinations=Fullterton,CA|Cerritos,CA&key=AIzaSyCYsJ9eFoI6lNx_gPuOoz7_j40lgv5X3QE`);
+    const origin = '9979 Muirlands Blvd, Irvine, CA 92618';
+    const destinations = 'Fullerton, CA|Cerritos, CA| Newport Beach, CA';
+    const request = await axios.get(`https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${origin}&destinations=${destinations}&key=${process.env.GOOGLE_API_KEY}`);
     const { data } = request;
     res.send(data);
   });
