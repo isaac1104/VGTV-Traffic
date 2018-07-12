25e4393ebb629b5a89d92aad4f4cef41b1dabcc2
Isaac Kwon <isaac-kwon@hotmail.com>
2018-07-12 14:02:32 -0700

    more destinations added

--- routes/distance_api_routes.js
+++ routes/distance_api_routes.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 module.exports = app => {
   app.get('/api/distance', async (req, res) => {
     const origin = '9979 Muirlands Blvd, Irvine, CA 92618';
-    const destinations = 'Fullerton, CA | Cerritos, CA | Newport Beach, CA | Los Angeles, CA | Santa Ana, CA | Rancho Palos Verdes, CA';
+    const destinations = 'Irvine, CA | Mission Viejo, CA | Newport Beach, CA | Santa Ana, CA | Fullerton, CA | Cerritos, CA | Los Angeles, CA';
     const request = await axios.get(`https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${origin}&destinations=${destinations}&key=${process.env.GOOGLE_API_KEY}`);
     const { data } = request;
     res.send(data);
